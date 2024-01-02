628a834eddc8dfd3384763b1b7889743cca4a66d
Khanh Chung <khanh.d.chung@gmail.com>
2024-01-01 20:26:02 -0800

    update logo path

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,12 +1,13 @@
 import { Outlet, Link } from 'react-router-dom';
 import { CountryProvider } from '../context/CountryContext';
+import logo from '../assets/Logo.svg';
 
 export default function root() {
   return (
     <div className="mx-auto bg-auto bg-top bg-no-repeat h-screen max-w-[1280px] bg-hero-img md:px-6 xl:px-10">
       <Link to="/">
         <div className="flex justify-center h-[240px]">
-            <img className="xl:mt-6" src="../src/assets/Logo.svg" width="174" height="24" alt="Logo" />
+            <img className="xl:mt-6" src={logo} width="174" height="24" alt="Logo" />
         </div>
       </Link>
       <CountryProvider>
