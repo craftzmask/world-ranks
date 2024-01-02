06a46f6c4eb10c6205107af5f1c41a7e92a9011c
Khanh Chung <khanh.d.chung@gmail.com>
2024-01-01 20:08:33 -0800

    fix relative path for logo

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,7 +6,7 @@ export default function root() {
     <div className="mx-auto bg-auto bg-top bg-no-repeat h-screen max-w-[1280px] bg-hero-img md:px-6 xl:px-10">
       <Link to="/">
         <div className="flex justify-center h-[240px]">
-            <img className="xl:mt-6" src="src/assets/Logo.svg" width="174" height="24" alt="Logo" />
+            <img className="xl:mt-6" src="../src/assets/Logo.svg" width="174" height="24" alt="Logo" />
         </div>
       </Link>
       <CountryProvider>
